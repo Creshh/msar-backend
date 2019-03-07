c5a7ef30440420ec29d47a048ef394bfb516d811
Tom Kretzschmar <tkretzschmar94@googlemail.com>
2019-03-07 13:09:57 +0100

    restructuring; move towards key-value store

--- src/main/java/de/tuchemnitz/tomkr/msar/Config.java
+++ src/main/java/de/tuchemnitz/tomkr/msar/Config.java
@@ -8,16 +8,19 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "de.tuchemnitz.tomkr.msar")
+//@EnableElasticsearchRepositories(basePackages = "de.tuchemnitz.tomkr.msar")
 //@EnableMongoRepositories(basePackages = "de.tuchemnitz.tomkr.metaapp.service.mongo")
 public class Config {
+	
+	private static Logger LOG = LoggerFactory.getLogger(Config.class);
+
 
 	@Value("${elasticsearch.host}")
 	public String host;
@@ -28,12 +31,20 @@ public class Config {
 	@Value("${elasticsearch.clustername}")
 	public String clusterName;
 	
+	private Client client;
 	
 	@Bean
-	public Client client() throws UnknownHostException {
-		Settings settings = Settings.builder().put("cluster.name", clusterName).build();
-		TransportClient client = new PreBuiltTransportClient(settings);
-		client.addTransportAddress(new TransportAddress(InetAddress.getByName(host), port));
-		return client;
+	public Client getClient() {
+		if(client == null) {
+			Settings settings = Settings.builder().put("cluster.name", clusterName).build();
+			TransportClient client = new PreBuiltTransportClient(settings);
+			try {
+				client.addTransportAddress(new TransportAddress(InetAddress.getByName(host), port));
+			} catch (UnknownHostException e) {
+				LOG.error("Error while retrieving host " + host, e);
+			}
+			this.client = client;
+		}
+		return this.client;
 	}
 }
---
+++ src/test/java/de/tuchemnitz/tomkr/msar/reader/ElasticServiceTest.java
@@ -0,0 +1,88 @@
+package de.tuchemnitz.tomkr.msar.reader;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.tuchemnitz.tomkr.msar.search.ElasticService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticServiceTest {
+
+	private static Logger LOG = LoggerFactory.getLogger(ElasticServiceTest.class);
+
+	@Autowired
+	ElasticService elastic;
+
+	@Autowired
+	JsonHelper json;
+	
+
+	@Before
+	public void before() {
+		elastic.deleteIndex();
+	}
+
+	
+	
+	@Test
+	public void testSaveAndRetrieve() throws InterruptedException {
+		LOG.debug("------------------------ testSaveAndRetrieve ------------------------ ");
+		String basePath = "D:\\ws\\eclipse_ws\\metaapp2\\src\\test\\resources\\jsonExamples\\";
+		
+		Map<String, String> metaData = new HashMap<>();
+		metaData.put("exifExample1.json", "exif");
+		metaData.put("exifExample2.json", "exif");
+		metaData.put("exifExample3.json", "exif");
+		metaData.put("objectsExample1.json", "objects");
+		metaData.put("objectsExample2.json", "objects");
+		metaData.put("locationExample1.json", "location");
+		metaData.put("locationExample2.json", "location");
+		metaData.put("locationExample3.json", "location");
+		
+		for(Entry<String, String> entry : metaData.entrySet()) {
+			Map<String, Object> map = json.readJsonToMap(basePath + entry.getKey());
+			elastic.indexDocument(map);
+		}
+		
+		Thread.sleep(5000);
+		LOG.debug("------------------------ indexing done ------------------------");
+		
+//		elastic.searchAll();
+		
+		elastic.searchByValue("Nikon");
+		elastic.searchByValue("Canon");
+		elastic.searchByValue("EF50mm + Nikon");
+		elastic.searchByValue("EF50mm + -Nikon");
+		
+		elastic.searchByValue("chair");
+		elastic.searchByValue("chair + table");
+		elastic.searchByValue("type:objects + -table");
+		
+		elastic.getSuggestions("Dresde", "city.completion");
+		elastic.getSuggestions("Dre", "city.completion");
+		elastic.getSuggestions("D", "city.completion");
+		
+		elastic.getSuggestions("Der", "city.completion");
+		elastic.getSuggestions("Dersden", "city.completion");
+		elastic.getSuggestions("Dersdi", "city.completion");
+		elastic.getSuggestions("B", "city.completion");
+		
+		
+		elastic.getSuggestions("Cha", "objects.completion");
+		elastic.getSuggestions("Ta", "objects.completion");
+		
+		LOG.debug("--------------------------------------------------------------------- ");
+	}
+}
