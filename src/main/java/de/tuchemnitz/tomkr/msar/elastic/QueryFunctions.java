dc3aca568f514627b7b9ae2d59062450e0dc1239
Tom Kretzschmar <tkretzschmar94@googlemail.com>
2019-03-20 11:12:30 +0100

    query returns reference list only

--- src/main/java/de/tuchemnitz/tomkr/msar/elastic/QueryFunctions.java
+++ src/main/java/de/tuchemnitz/tomkr/msar/elastic/QueryFunctions.java
@@ -79,9 +79,17 @@ public class QueryFunctions {
 		return search(QueryBuilders.rangeQuery(field).from(lower, true).to(upper, true), indices);
 	}
 
-	public List<Map<String, Object>> searchByValue(String value, String... indices) {
+	public List<String> searchByValue(String value, String... indices) {
 		LOG.debug(String.format("Search of [%s] in all fields", value));
-		return search(QueryBuilders.queryStringQuery(value), indices);
+		
+		List<Map<String, Object>> results = search(QueryBuilders.queryStringQuery(value), indices);
+		
+		List<String> result = new ArrayList<>();
+		for(Map<String, Object> entry : results) {
+			result.add((String) entry.get(FIELD_REFERENCE));
+		}
+		
+		return result;
 	}
 
 	public Map<String, SuggestCategory> getSuggestions(String value) {
